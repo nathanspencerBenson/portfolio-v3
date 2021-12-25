9cf612db786f22528a2bd0ea938937d769436818
Nathan <nathanbenson.affiliate@gmail.com>
2021-12-25 23:30:25 +0100

    created landingPage

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Nav.scss';
+import {FiGithub} from 'react-icons/fi';
+
+function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li><a href='#'>Home</a><div className="border" style={{transform: 'rotate(3deg)'}}></div></li>
+                <li><a href='#'>Projects</a><div className="border" style={{transform: 'rotate(-3deg)'}}></div></li>
+                <li><a href='#'>About</a><div className="border" style={{transform: 'rotate(3deg)'}}></div></li>
+                <li><a href='#'>Contact</a><div className="border" style={{transform: 'rotate(-3deg)'}}></div></li>
+            </ul>
+            <div className="logo"></div>
+            <div>
+                <a href='https://github.com/nathanspencerBenson'><FiGithub size={25} />Github </a>
+            </div>
+            
+        </nav>
+    )
+}
+
+export default Nav
